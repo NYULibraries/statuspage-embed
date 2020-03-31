4d9a8bc74fcc1fceaeb22e15ce5b813013be9186
Eric Griffis <eric.griffis@nyu.edu>
2020-03-31 13:43:35 -0400

    Add support for jasmine to linter

--- spec/alertBannerSpec.js
+++ spec/alertBannerSpec.js
@@ -1,29 +1,30 @@
 // https://jasmine.github.io/api/3.5/global
 // https://jasmine.github.io/pages/docs_home.html
+//
+import jsdom from 'jsdom';
 
 // Include the alertBanner module
-import * as alertBanner from "../js/alertBanner.js";
+import * as alertBanner from '../js/alertBanner';
 
 // Mock the DOM with JSDOM
 // https://github.com/jsdom/jsdom
-import jsdom from 'jsdom';
 const { JSDOM } = jsdom;
-const { document } = (new JSDOM(`<!DOCTYPE html><html><head></head><body><div class="container"></div></body></html`)).window;
+const { document } = (new JSDOM(`<!DOCTYPE html><html><head></head><body><div class='container'></div></body></html`)).window;
 global.document = document;
 
-describe("alertBanner", () => {
+describe('alertBanner', () => {
 
-  xdescribe("#bannerContent", () => {
-    it("is expected to be static content", () => {
+  xdescribe('#bannerContent', () => {
+    it('is expected to be static content', () => {
       expect(alertBanner.bannerContent()).toBe('As of March 14, NYU Libraries services are all online. Library spaces are closed until further notice. <a href="https://nyulibraries.statuspage.io" target="_blank">See more</a>');
     });
   });
 
-  describe("#insertBanner", () => {
+  describe('#insertBanner', () => {
     beforeEach(() => {
       alertBanner.insertBanner();
     });
-    it("is expected to insert the banner element", () => {
+    it('is expected to insert the banner element', () => {
       expect(document.body.children.length).toBe(2);
     });
   });
