d1318f4c5adf81961c9762970b755f91216dbc31
Eric Griffis <eric.griffis@nyu.edu>
2020-05-06 10:58:21 -0400

    Add comments

--- js/alertBanner.js
+++ js/alertBanner.js
@@ -1,6 +1,7 @@
 import StatuspageApi from './statuspageApi';
 import config from './config';
 
+// helper to create element with attributes; not exported or independently tested
 const createElementWithAttrs = (tagName, content, attributes) => {
   const elem = document.createElement(tagName);
   Object.keys(attributes).forEach((attr) => {
@@ -15,11 +16,13 @@ class AlertBanner {
     this.statuspage = new StatuspageApi();
   }
 
+  // returns class string with appropriate color class for set status
   bannerClass() {
     const color = config.statusToColorMapping[this.lastStatus];
     return `nyulibraries-alert-banner alert-${color}`;
   }
 
+  // inserts banner with message and appropriate color from bannerClass
   insertBanner() {
     const alertBannerDiv = createElementWithAttrs('aside', `${this.message} `, {
       id: 'nyulibraries-alert-banner',
@@ -31,6 +34,7 @@ class AlertBanner {
     return document.body.insertBefore(alertBannerDiv, document.body.firstChild);
   }
 
+  // inserts stylesheet to document
   static insertStylesheet() {
     const linkDiv = document.createElement('link');
     linkDiv.setAttribute('rel', 'stylesheet');
@@ -39,6 +43,7 @@ class AlertBanner {
     return document.head.appendChild(linkDiv);
   }
 
+  // initialize banner: pull latest statuspage data, and insert if matching matching hastag
   async init() {
     this.constructor.insertStylesheet();
     await this.statuspage.getData();
