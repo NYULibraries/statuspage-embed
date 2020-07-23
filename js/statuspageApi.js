4ccfff9b3ccd2341db5b299f09b77bce46bb143b
laurxnemeth <lauranemeth.pr@gmail.com>
2020-07-23 15:50:19 -0400

    Add support for if there are no maintenances

--- js/statuspageApi.js
+++ js/statuspageApi.js
@@ -1,6 +1,8 @@
 import config from './config';
 
-const hashtagRegexp = new RegExp('#(majoroutage|weatherclosure|buildingclosure|scheduledmaintenance)');
+const hashtagRegexp = new RegExp(
+  '#(majoroutage|weatherclosure|buildingclosure|scheduledmaintenance)'
+);
 
 class StatuspageApi {
   async getData() {
@@ -12,27 +14,32 @@ class StatuspageApi {
     let chosenIncident = undefined;
     let incidents = this.data.incidents[0];
     let scheduledMaintenances;
+    let incidentUpdated;
+    let maintenancesUpdated;
 
     // if scheduled_maintenances has a relevant hashtag, add to the scheduledMaintenances variable
     // if not, the chosenIncident is automatically incidents
-    hashtagRegexp.test(
-      this.data.scheduled_maintenances[0].incident_updates[0].body
-    )
-      ? (scheduledMaintenances = this.data.scheduled_maintenances[0])
-      : (chosenIncident = incidents);
+    if (
+      this.data.scheduled_maintenances === undefined ||
+      !hashtagRegexp.test(
+        this.data.scheduled_maintenances[0].incident_updates[0].body
+      )
+    ) {
+      chosenIncident = incidents;
+    } else {
+      scheduledMaintenances = this.data.scheduled_maintenances[0];
+    }
 
-    // if chosenIncident is still undefined
     if (chosenIncident === undefined) {
 
       // check if they were updated at the same time
-      if (incidents.updated_at == scheduledMaintenances.updated_at){
-        chosenIncident = incidents
+      if (incidents.updated_at === scheduledMaintenances.updated_at) {
+        chosenIncident = incidents;
       } else {
-
         // else, check which one was the most recent
         incidents.updated_at < scheduledMaintenances.updated_at
-        ? (chosenIncident = scheduledMaintenances)
-        : (chosenIncident = incidents);
+          ? (chosenIncident = scheduledMaintenances)
+          : (chosenIncident = incidents);
       }
     }
     return chosenIncident;
