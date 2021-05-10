74ba5e9f642cc1ffd5fda0027655315c6fbcc0a0
Eric Griffis <eric.griffis@nyu.edu>
2021-05-10 11:44:36 -0400

    Webpack - remove output config to break build

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,6 @@ module.exports = (env) => {
         './scss/index.scss',
       ],
     },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].min.js',
-    },
     devtool: isProduction || isStaging ? 'source-map' : 'eval-source-map',
     module: {
       rules: [
